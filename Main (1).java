b5c87a5b58091db1dcaa208a5f7be7f29f772a95
UnknownJavaDawn <deejaypulidoaguares14@gmail.com>
2024-12-10 15:23:34 +0800

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,52 @@
+// First Programming Activity
+// Dawn Jazhere P. Aguares
+// September 3, 2024
+
+public class Main {
+  public static void main(String[] args) {
+    //it displays the text inside the ""
+    //\t means tab
+    //\n means new line
+    
+    System.out.println("Dawn Jazhere P. Aguares");
+    System.out.println("Hello World!");
+    System.out.println("CC2 - Introduction to Programming");
+    System.out.println("\n\n");
+    System.out.println("\t\t\t_________________________\n");
+    System.out.println("\t\t\tDokki\"s Groceries");
+    System.out.println("\t\t\t_________________________________________");
+
+    /* This is a multi-line comment 
+    
+    \t is an escape sequence i.e. simply a special character that allows you to insert a tab space between two words.
+    
+    \n means a newline, like when you press the return or enter key.
+    */
+    System.out.println("\n");
+    
+    System.out.println("Items\t\t\t\t\t\t\t\tQuantity\t\t\t\t\t\tPrice\n");
+    
+    System.out.println("Apples:\t\t\t\t\t\t\t\t2\t\t\t\t\t\t\t\t10 (each)\n");
+    
+    System.out.println("KiwiFruits:\t\t\t\t\t\t\t3\t\t\t\t\t\t\t\t14 (each)\n");
+    
+    System.out.println("Bananas:\t\t\t\t\t\t\t4\t\t\t\t\t\t\t\t12 (each)\n");
+    
+    System.out.println("Pears:\t\t\t\t\t\t\t\t5\t\t\t\t\t\t\t\t20 (each)\n");
+    
+    System.out.println("Oranges:\t\t\t\t\t\t\t6\t\t\t\t\t\t\t\t10 (each)\n");
+      
+
+    System.out.println("\t\t\t____________________________________________________\n");
+    System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\nTotal amount:\t\t\t\t");
+    System.out.println(2*10+3*14+4*12+5*20+6*10);
+    System.out.println("\t\t\t____________________________________________________\n");
+    
+    System.out.println("\t\t\t\t\t\t\tThank you!");
+      //Thank you!
+
+    
+  }
+
+  
+}
