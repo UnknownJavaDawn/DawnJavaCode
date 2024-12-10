047b1d1e75c71e95f2823510b54fcbb508ce84a9
UnknownJavaDawn <deejaypulidoaguares14@gmail.com>
2024-12-10 15:22:52 +0800

    Add files via upload

---
+++ LaboratoryChallenge2.java
@@ -0,0 +1,24 @@
+// Second Programming Activity
+// DAWN JAZHERE P. AGUARES
+// September 7, 2024
+
+
+public class LaboratoryChallenge2 {
+        public static void main(String[] args){
+        
+        //
+        
+        int length = 10;
+        
+        int width = 5;
+        
+        int area = length * width;
+        int perimeter = 2 * (length + width);
+        
+        System.out.println("Length of the garden:" + length + " meters");
+        System.out.println("Width of the garden:" + width + " meters");
+        System.out.println("Area of the garden:" + area + " square meters");
+        System.out.println("Permiter of the garden:" + length + " meters");
+        }
+        
+}
